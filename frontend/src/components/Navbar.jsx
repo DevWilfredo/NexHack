afd5448153d431512853f0c476906b12926c21ba
waloleitor <tsarciatroch@gmail.com>
2025-07-12 12:34:45 +0200

    Creacion de Layout, Footer y Navbar, reajustes en jsx pertinentes.

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-200 shadow-md px-4">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl" href="/">NEXHACK</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a href="/">Inicio</a></li>
+                    <li><a href="/login">Login</a></li>
+                    <li><a href="/dashboard">Dashboard</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -5,10 +5,10 @@ import InputComponent from "../../components/Input";
 
 const Home = () => {
   return (
-    <div>
-      <ButtonComponent />
+    <div className="flex flex-col items-center gat-4 p-6">
       <Avatar />
       <InputComponent />
+      <ButtonComponent />
     </div>
   );
 };
