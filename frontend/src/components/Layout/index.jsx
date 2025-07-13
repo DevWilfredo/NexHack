81f606504f5ec8fbe5e983568a153cb0b012757e
waloleitor <tsarciatroch@gmail.com>
2025-07-13 19:08:17 +0200

    Revisión 2 de imports

--- frontend/src/components/Layout/index.jsx
+++ frontend/src/components/Layout/index.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../Navbar";
-import Footer from "../Footer/Footer";
+import Navbar from "../Navbar/index";
+import Footer from "../Footer/index";
 
 const Layout = () => {
 return (
