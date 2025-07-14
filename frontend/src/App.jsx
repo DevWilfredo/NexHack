65255f048a848fbd30a846d944787dd715c03b6b
Ghostskoll <62819017+Ghostskoll@users.noreply.github.com>
2025-07-14 20:44:28 +0200

    Added Routes to work with

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
-import { Route, Routes } from 'react-router'
-import Home from './pages/Home'
-import Layout from './components/Layout'
+import React from "react";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Layout from "./components/Layout";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import UserProfile from "./pages/UserProfile";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/profile" element={<UserProfile />} />
       </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default App
+export default App;
