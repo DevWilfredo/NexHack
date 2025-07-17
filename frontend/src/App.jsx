49c13b8701a5718892f44b9961de275279121b4e
Wilfredo Pinto <wilfredopintomata@gmail.com>
2025-07-17 22:25:29 +0200

    favicon change v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router";
-import FaviconUpdater from "./components/FaviconUpdater";
 import ShiftingCountdown from "./components/AnimatedCountdown";
 
 const App = () => {
   return (
     <>
-      <FaviconUpdater />
       <Routes>
         <Route path="/" element={<ShiftingCountdown />} />
         <Route path="*" element={<Navigate to="/" replace />} />
