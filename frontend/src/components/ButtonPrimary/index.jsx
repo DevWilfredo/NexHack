dafb6f2da3dab8ab2a8b047ee26cab60681cbe98
waloleitor <tsarciatroch@gmail.com>
2025-07-17 18:55:44 +0200

    implementada vista de login con componentes reutilizables

--- frontend/src/components/ButtonPrimary/index.jsx
+++ frontend/src/components/ButtonPrimary/index.jsx
@@ -1,9 +1,13 @@
 import { useTheme } from "../../context/ThemeContext";
 
-const ButtonPrimary = ({ title, icon: Icon, className = "" }) => {
+const ButtonPrimary = ({ title, icon: Icon, className = "", type="button", onClick }) => {
   const { isDark } = useTheme();
+
   return (
-    <button className={`btn btn-${isDark ? 'accent' : 'primary'} ${className}`}>
+    <button 
+    type={type}
+    onClick={onClick}
+    className={`btn btn-${isDark ? 'accent' : 'primary'} ${className}`}>
       {title}
       {Icon && <Icon className="w-4 h-4 ml-1" />}
     </button>
--- frontend/src/components/Input/index.jsx
+++ frontend/src/components/Input/index.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-const InputComponent = () => {
+const InputComponent = ({label, name, type = "text", placeholder, value, onChange}) => {
   return (
-    <fieldset className="fieldset">
-      <legend className="fieldset-legend">What is your name?</legend>
-      <input type="text" className="input" placeholder="Type here" />
-      <p className="label">Optional</p>
+    <fieldset className="fieldset mb-4">
+      <legend className="fieldset-legend mb-1">{label}</legend>
+      <input 
+      type={type}
+      name={name} 
+      className="input input-bordered w-full" 
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      />
     </fieldset>
   );
 };
