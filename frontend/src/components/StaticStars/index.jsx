4cf4e96219764e7dabeeb8e7c6a64ca844d53d86
Ghostskoll <62819017+Ghostskoll@users.noreply.github.com>
2025-08-05 15:46:55 +0200

    Stars bug on movile Fixed

--- frontend/src/components/StaticStars/index.jsx
+++ frontend/src/components/StaticStars/index.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useId } from "react";
 
-function StaticStarsComponent({ value }) {
+function StaticStarsComponent({ value, small = false }) {
+  const id = useId(); // <-- genera ID único por instancia
+  const ratingSizeClass = small
+    ? "rating-sm md:rating-md"
+    : "rating-lg md:rating-lg";
+
   return (
-    <div className="rating rating-lg rating-half pointer-events-none select-none">
+    <div
+      className={`rating rating-half ${ratingSizeClass} pointer-events-none select-none`}
+    >
       {[...Array(10)].map((_, i) => {
         const half = i % 2 === 0 ? "mask-half-1" : "mask-half-2";
-        const starValue = (i + 1) / 2; // (0.5 to 5.0)
-        const checked = value / 2 === starValue;
+        const starValue = (i + 1) / 2;
+        const checked = Math.abs(value / 2 - starValue) < 0.01;
 
         return (
           <input
             key={i}
             type="radio"
-            name={`readonly-stars-${value}`} // unique name to avoid conflict
+            name={`readonly-stars-${value}-${id}`} // ✅ name único por instancia
             className={`mask mask-star-2 ${half} bg-warning`}
             aria-label={`${starValue} star`}
             disabled
